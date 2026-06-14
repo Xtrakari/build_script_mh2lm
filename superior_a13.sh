2a132966aa5584c251baaece3821fd9e91ffebd7
Xtrakari <nsrakip@gmail.com>
2026-06-14 22:27:07 +0800

    Create superior_a13.sh

---
+++ superior_a13.sh
@@ -0,0 +1,37 @@
+rm -rf .repo/local_manifests/
+rm -rf device/lge
+rm -rf kernel/lge
+rm -rf vendor/lge
+rm -rf hardware/lge
+rm -rf build/soong
+rm -rf out/target/product/mh2lm
+# Cleanup previous changelog to make it always fresh
+rm -rf out/target/product/*/system/etc/Changelog.txt \
+       out/target/product/*/obj/ETC/Changelog.txt_intermediates \
+       out/target/product/*/gen/ETC/Changelog.txt_intermediates
+
+# Clone evox
+
+repo init -u https://github.com/Superior13-NEXT/manifest.git -b QPR3 --depth=1 --git-lfs
+#Temp Fix Repo tool
+#cd .repo/repo;git pull -r;cd ../..;
+
+# Clone local_manifests repository
+git clone https://github.com/Xtrakari/local_manifest_mh2lm.git --depth 1 -b main .repo/local_manifests
+if [ ! 0 == 0 ]
+ then   curl -o .repo/local_manifests https://github.com/Xtrakari/local_manifest_mh2lm.git
+ fi
+
+# repo sync
+/opt/crave/resync.sh
+
+source build/envsetup.sh
+
+# brunch configuration
+lunch superior_mh2lm-userdebug
+
+# Clean
+make installclean
+
+# Run
+m bacon
