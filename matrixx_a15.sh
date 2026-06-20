9d3244399fdef6bf9e485d5227f499631d801719
Xtrakari <nsrakip@gmail.com>
2026-06-20 21:33:46 +0800

    Update and rename matrixx_a14.sh to matrixx_a15.sh

--- matrixx_a15.sh
+++ matrixx_a15.sh
@@ -11,19 +11,16 @@ rm -rf out/target/product/*/system/etc/Changelog.txt \
        out/target/product/*/gen/ETC/Changelog.txt_intermediates
 
 # Clone axion
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --depth=1 --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --depth=1 --git-lfs
 
 # Clone local_manifests repository
 git clone https://github.com/Xtrakari/local_manifest_mh2lm.git --depth 1 -b mh2lm-matrixx .repo/local_manifests
 
 # repo sync
-repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) 
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # Initialize the build environment variables
 source build/envsetup.sh
-
-# Clean up the previous target files safely before compiling
-make installclean
 
 # Run the build using Brunch mode
 brunch mh2lm
