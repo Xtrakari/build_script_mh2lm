19980ad6b6ae36c64076a34f2aba2591bbe2635b
Xtra <nsrakip@gmail.com>
2026-08-16 13:12:37 +0800

    Create lunaris_a16.sh

---
+++ lunaris_a16.sh
@@ -0,0 +1,13 @@
+rm -rf .repo/local_manifests/
+
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs --depth=1
+
+git clone https://github.com/Xtrakari/local_manifest_mh2lm/blob/mh2lm-lunaris16/local_manifest.xml .repo/local_manifests -b mh2lm-lunaris16 
+
+/opt/crave/resync.sh
+
+source build/envsetup.sh
+
+lunch lineage_mh2lm-bp4a-userdebug
+
+m bacon
