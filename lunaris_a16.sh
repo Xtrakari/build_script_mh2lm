80b8f44bb01f27fc94185626d3e362faf4208e21
Xtra <nsrakip@gmail.com>
2026-08-16 22:52:33 +0800

    Update lunaris_a16.sh

--- lunaris_a16.sh
+++ lunaris_a16.sh
@@ -2,7 +2,7 @@ rm -rf .repo/local_manifests/
 
 repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs --depth=1
 
-git clone https://github.com/Xtrakari/local_manifest_mh2lm.git .repo/local_manifests -b mh2lm-lunaris16 
+git clone https://github.com/Xtrakari/local_manifest_mh2lm.git --depth 1 -b mh2lm-lunaris16 .repo/local_manifests
 
 /opt/crave/resync.sh
 
