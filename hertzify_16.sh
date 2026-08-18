0be989391f25081a18197427d5921e6b3fe631f7
Xtra <nsrakip@gmail.com>
2026-08-19 00:06:08 +0800

    Update hertzify_16.sh

--- hertzify_16.sh
+++ hertzify_16.sh
@@ -1,1 +1,30 @@
+rm -rf .repo/local_manifests/
+rm -rf device/lge
+rm -rf kernel/lge
+rm -rf vendor/lge
+rm -rf hardware/lge
+rm -rf out/target/product/mh2lm
+# Cleanup previous changelog to make it always fresh
+rm -rf out/target/product/*/system/etc/Changelog.txt \
+       out/target/product/*/obj/ETC/Changelog.txt_intermediates \
+       out/target/product/*/gen/ETC/Changelog.txt_intermediates
+
+# Clone evox
+
+repo init -u https://github.com/HertzifyOS/android_manifest.git -b 16 --depth=1 --git-lfs --no-clone-bundle
+
+git clone https://github.com/Xtrakari/local_manifest_mh2lm.git --depth 1 -b mh2lm-hertzify16 .repo/local_manifests
+if [ ! 0 == 0 ]
+ then   curl -o .repo/local_manifests https://github.com/Xtrakari/local_manifest_mh2lm.git
+ fi
+
+/opt/crave/resync.sh
+
+source build/envsetup.sh
+
+lunch hertzify_mh2lm-bp4a-user
+
+make installclean
+
+mka bacon
 
