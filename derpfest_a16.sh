a6c01ce50706d59b70325abe16b8f9871fe47327
Xtra <nsrakip@gmail.com>
2026-08-22 23:22:07 +0800

    Update derpfest_a16.sh

--- derpfest_a16.sh
+++ derpfest_a16.sh
@@ -1,1 +1,16 @@
 
+rm -rf .repo/local_manifests/
+
+repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16.2 --git-lfs --depth=1
+
+git clone https://github.com/Xtrakari/local_manifest_mh2lm.git --depth 1 -b mh2lm-derpfest16 .repo/local_manifests
+
+/opt/crave/resync.sh
+
+source build/envsetup.sh
+
+lunch lineage_mh2lm-bp4a-userdebug
+
+m installclean
+
+mka derp
