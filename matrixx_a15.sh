038a8a5d344aff75e97ea54dd5a77a0612621ee1
Xtra <nsrakip@gmail.com>
2026-08-26 20:05:20 +0800

    Update matrixx_a15.sh

--- matrixx_a15.sh
+++ matrixx_a15.sh
@@ -3,24 +3,20 @@ rm -rf device/lge
 rm -rf kernel/lge
 rm -rf vendor/lge
 rm -rf hardware/lge
-rm -rf build/soong
 rm -rf out/target/product/mh2lm
 # Cleanup previous changelog to make it always fresh
 rm -rf out/target/product/*/system/etc/Changelog.txt \
        out/target/product/*/obj/ETC/Changelog.txt_intermediates \
        out/target/product/*/gen/ETC/Changelog.txt_intermediates
 
-# Clone axion
-repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --depth=1 --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android -b 16.2 --depth=1 --git-lfs --no-clone-bundle
 
-# Clone local_manifests repository
-git clone https://github.com/Xtrakari/local_manifest_mh2lm.git --depth 1 -b mh2lm-matrixx .repo/local_manifests
+git clone https://github.com/Xtrakari/local_manifest_mh2lm.git --depth 1 -b mh2lm-matrixx16 .repo/local_manifests
 
-# repo sync
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+/opt/crave/resync.sh
 
-# Initialize the build environment variables
 source build/envsetup.sh
 
-# Run the build using Brunch mode
-brunch mh2lm
+lunch matrixx_mh2lm-bp4a-userdebug
+
+make matrixx
